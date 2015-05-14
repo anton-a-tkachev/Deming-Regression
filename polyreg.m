df486acb6b0b8fdafdbd45fbfe9d10d02be9899e
Anton Tkachev <anton.a.tkachev@gmail.com>
2015-05-14 10:45:26 +1000

    Create polyreg.m

---
+++ polyreg.m
@@ -0,0 +1,27 @@
+function [r, a, c, s] = polyreg(x,y,p)
+%POLYREG Polynomial regression
+%   Inputs
+%   x, y - dataset vectors
+%   p - polynomial order
+%   Outputs
+%   r - regression points
+%   a - polynomial coefficients array
+%   c - correlation between the regression line and the points of dataset
+%   s - residual standard deviation calculated from regression line
+
+n = length(x);
+X = zeros(n,p+1);
+for i=1:n
+     X(i,1) = 1;
+    for j=1:p
+        X(i,j+1) = x(i)^j;
+    end
+end
+
+a = (X'*X)\X.'*y;
+r = X*a;
+s = std(y - r);
+c = 1 - (s/std(y))^2;
+
+end
+
